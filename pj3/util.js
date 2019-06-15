8d3d85cf6fd02cfe499906df43b39cd4fc6f1f27
zzxn <zzxnhackman@gmail.com>
2019-06-15 22:04:11 +0800

    fix: use mipmap

--- pj3/util.js
+++ pj3/util.js
@@ -171,8 +171,10 @@ function initTexture(gl, program, texImagePath) {
         gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);  // flip the image Y coordinate
         gl.activeTexture(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+        gl.generateMipmap(gl.TEXTURE_2D);
 
         // pass the texture unit 0 to u_Sampler
         gl.useProgram(program);
